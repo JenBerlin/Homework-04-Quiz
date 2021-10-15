9ec5c4b073c434dd583f9a354da61f578eb12c94
Jenny Tschauschew <kaesekuchen123@yahoo.de>
2021-10-15 18:01:01 +0200

    adjustments html style script

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
+var startBtn = document.querySelector("#start");
 var timeEl = document.querySelector(".time")
-var secondsLeft = 20
+var gameTime = 10
+var questionContainerElement = document.querySelector("#question-container")
 
-function setTime () {
-    var timerInterval = setInterval(function() {
-        secondsLeft--;
+var timer;
+
+function startQuiz() {
+    
+
+}
+
+
+
+function startTimer() {
+    var count = 0;
+    timer = setInterval(function() {
+        count++;
+        var secondsLeft = gameTime - count;
         timeEl.textContent = "Time: " + secondsLeft;
 
-        if(secondsLeft === 0) {
-            clearInterval(timerInterval);
-            sendMessage();
+        if(count >= gameTime) {
+            finishTheGame();
         }
     }, 2000)
+}
+
+function finishTheGame() {
+    clearInterval(timer);
+    // if all questions are correct
+        // all correct needs to be a function
+    // + update Score 
 }
 
 function sendMessage() {
     timeEl.textContent = "Game over"
 }
+
+startBtn.addEventListener("click", startQuiz);
+

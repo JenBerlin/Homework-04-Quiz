f0516a0cd832954e98e6975bf9e78316c3984d91
Jenny Tschauschew <kaesekuchen123@yahoo.de>
2021-10-15 20:25:04 +0200

    add index script style

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-var startBtn = document.querySelector("#start");
-var timeEl = document.querySelector(".time")
+var startBtn = document.getElementById("start")
+var timeEl = document.getElementById("time")
 var gameTime = 10
+var landingPage = document.getElementById("landing-page")
 var questionContainerElement = document.querySelector("#question-container")
+var container1 = document.getElementById("question-container1")
+var container2 = document.getElementById("question-container2")
+var container3 = document.getElementById("question-container3")
+var emailContainer = document.getElementById("email-container")
+var scorelContainer = document.getElementById("score-container")
 
 var timer;
+var score = 0
 
-function startQuiz() {
-    
-
-}
-
-
+container1.style.display = "none"
+container2.style.display = "none"
+container3.style.display = "none"
+emailContainer.style.display = "none"
+scorelContainer.style.display = "none"
 
 function startTimer() {
     var count = 0;
@@ -20,9 +26,41 @@ function startTimer() {
         timeEl.textContent = "Time: " + secondsLeft;
 
         if(count >= gameTime) {
+            timeEl.textContent = "Time: " + 0;
             finishTheGame();
         }
-    }, 2000)
+    }, 1000)
+}
+
+function handle1(e) {
+    console.log(e.target.id)
+    var answer = e.target.id
+    if(answer === "a1-3"){
+        score += 10
+        container1.style.display = "none"
+        container2.style.display = "block"
+    } 
+    else {
+        score -= 5
+        document.getElementById(answer).style.backgroundColor = "red"
+    }
+}
+console.log(score)
+
+function handle2() {
+    
+}
+
+function handle3() {
+    
+}
+
+function startQuiz() {
+    startTimer()
+    landingPage.style.display = "none"
+    container1.style.display = "block"
+
+    
 }
 
 function finishTheGame() {
@@ -37,4 +75,5 @@ function sendMessage() {
 }
 
 startBtn.addEventListener("click", startQuiz);
+console.log
 
